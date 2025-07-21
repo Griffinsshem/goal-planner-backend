8ba3f95b27705cbd1676b27492298649f084da9e
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-07-21 16:06:58 +0300

    Initial backend commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const jsonServer = require('json-server');
+const cor = require('cors');
+
+const server = jsonServer.create();
+
+server.use(cors());
+server.use(jsonServer.router('db.json'));
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
